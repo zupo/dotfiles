1d25a87a16ec33cd6642290df31ea662ca1c0ff6
Nejc Zupan <nejc.zupan@gmail.com>
2022-06-09 07:47:03 +0200

    upgrade darwin-nix to latest master

--- pins/darwin.nix
+++ pins/darwin.nix
@@ -1,5 +1,5 @@
 fetchTarball {
-  # https://github.com/LnL7/nix-darwin/tree/master on 2021-12-06
-  url = "https://github.com/LnL7/nix-darwin/archive/44da835ac40dab5fd231298b59d83487382d2fab.tar.gz";
-  sha256 = "06nvl7d0zj2c02ax2v77580p8h0pjlrv1ah3qrl9c6lqadal4sf6";
+  # https://github.com/LnL7/nix-darwin/tree/master on 2022-06-08
+  url = "https://github.com/LnL7/nix-darwin/archive/2f2bdf658d2b79bada78dc914af99c53cad37cba.tar.gz";
+  sha256 = "07mjslw2hcsrhnvlwp0z5kn6dr3fk3ac2yj5ahp856q6wn8zvq2j";
 }
