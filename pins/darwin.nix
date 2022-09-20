1a9a594ff7a5d373a71551cc8f41f0201d645692
Nejc Zupan <nejc.zupan@gmail.com>
2022-09-20 14:54:39 +0100

    Update to latest nix-darwin master & nixpkgs 22.05

--- pins/darwin.nix
+++ pins/darwin.nix
@@ -1,5 +1,5 @@
 fetchTarball {
-  # https://github.com/LnL7/nix-darwin/tree/master on 2022-08-26
-  url = "https://github.com/LnL7/nix-darwin/archive/ef0e7f41cdf8fae1d2390c4df246c90a364ed8d9.tar.gz";
-  sha256 = "1zyrzhvpi0pykg0zcmr0sc43qclxj97xq2p8fl98sgh90y6cb9vm";
+  # https://github.com/LnL7/nix-darwin/tree/master on 2022-09-20
+  url = "https://github.com/LnL7/nix-darwin/archive/088c98a584a38b5f844bb9e9cd32eb28479ca6d7.tar.gz";
+  sha256 = "1lq1jk7qhg0pl8vgx12jr0q7wjg40as7b87bbl1zz3kbyi8jgx1m";
 }
--- pins/nixpkgs.nix
+++ pins/nixpkgs.nix
@@ -1,5 +1,5 @@
 fetchTarball {
-  # https://github.com/NixOS/nixpkgs/tree/nixos-22.05 on 2022-08-26
-  url = "https://github.com/nixos/nixpkgs/archive/058de3818577db19d1965c21e2479916a3eaaf95.tar.gz";
-  sha256 = "0zd7x7ar74v9jrd9gr9avf6na77qlqivyz8vg4zxsv4yp06r55rc";
+  # https://github.com/NixOS/nixpkgs/tree/nixos-22.05 on 2022-09-20
+  url = "https://github.com/nixos/nixpkgs/archive/20dc478985d6545df53f0153f4af125eb014083d.tar.gz";
+  sha256 = "0yl70fk9f174m1amkgkr1a2z1wds02zhsxaph7j7iy3wypy12bp0";
 }
