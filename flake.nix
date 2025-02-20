4dafe1ccd526bd7b778752338f5accea4af8785a
Neyts Zupan <zupo@users.noreply.github.com>
2025-02-20 08:26:51 +0100

    Follow stable branches for nixpkgs, nix-darwin and home-manager

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "zupo's macOS/Darwin system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
