f8665d037a594c4e2d946bf05ef583579c721ea7
Neyts Zupan <zupo@users.noreply.github.com>
2025-03-26 21:03:59 +0000

    add support for devenv

--- flake.nix
+++ flake.nix
@@ -298,10 +298,12 @@
         # Configure Cachix
         nix.settings.substituters = [
           "https://cache.nixos.org"
+          https://devenv.cachix.org
           "https://niteo.cachix.org"
         ];
         nix.settings.trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "niteo.cachix.org-1:GUFNjJDCE199FDtgkG3ECLrAInFZEDJW2jq2BUQBFYY="
         ];
         nix.settings.netrc-file = "/Users/zupo/.config/nix/netrc";
