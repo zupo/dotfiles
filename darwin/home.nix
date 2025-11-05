17762722b92b7503e92448921efe414e7d88bfe4
Neyts Zupan <zupo@users.noreply.github.com>
2025-11-05 11:26:26 +0000

    Test out Ghostty

--- darwin/home.nix
+++ darwin/home.nix
@@ -81,7 +81,7 @@
       #!/bin/bash
       # https://github.com/microsoft/vscode/issues/68579#issuecomment-463039009
       code --wait "$@"
-      open -a Terminal
+      open -a Ghostty
     '';
   };
 
